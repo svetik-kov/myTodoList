d1b48998d361b953adab301fe9401425d823392c
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-04-08 21:07:17 +0300

    Импорты с baseUrl

--- src/common/theme/theme.ts
+++ src/common/theme/theme.ts
@@ -1,5 +1,5 @@
 import {createTheme} from '@mui/material/styles'
-import type {ThemeMode} from '../../app/app-reducer'
+import type {ThemeMode} from '@/app/app-reducer.ts'
 
 export const getTheme = (themeMode: ThemeMode) => {
     return createTheme({
