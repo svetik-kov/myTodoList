9db647dfda1b4cab66de64aace68dc8b11dc2190
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-04-29 13:52:51 +0300

    RTK query . matchPending

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,5 +1,7 @@
 import { createSlice, isFulfilled, isPending, isRejected } from "@reduxjs/toolkit"
 import { RequestStatus } from "@/common/types"
+import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
+import { tasksApi } from "@/features/todolists/api/tasksApi.ts"
 
 export const appSlice = createSlice({
   name: "app",
@@ -11,9 +13,18 @@ export const appSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addMatcher(isPending, (state) => {
+      .addMatcher(isPending, (state, action) => {
+        if (
+          todolistsApi.endpoints.getTodolists.matchPending(action) ||
+          tasksApi.endpoints.getTasks.matchPending(action)
+        ) {
+          return
+        }
         state.status = "loading"
       })
+      /*.addMatcher(isPending, (state) => {
+        state.status = "loading"
+      })*/
       .addMatcher(isFulfilled, (state) => {
         state.status = "succeeded"
       })
