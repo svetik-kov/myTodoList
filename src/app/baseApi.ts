6c7dd0251a7839e0d94e50b3f1eca761ef97be83
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-04-30 10:27:37 +0300

    RTK query. Рефакторинг

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -3,6 +3,7 @@ import { AUTH_TOKEN } from "@/common/constants"
 import { setAppErrorAC } from "@/app/app-slice.ts"
 import { isErrorWithMessage } from "@/common/utils/isErrorWithMessage.ts"
 import { ResultCode } from "@/common/enums/enums.ts"
+import { handleError } from "@/common/utils"
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
@@ -23,43 +24,7 @@ export const baseApi = createApi({
       },
     })(args, api, extraOptions)
 
-    let error = "Some error occurred"
-
-    // 1. Global query errors
-    if (result.error) {
-      switch (result.error.status) {
-        case "FETCH_ERROR":
-        case "PARSING_ERROR":
-        case "CUSTOM_ERROR":
-          error = result.error.error
-          break
-
-        case 403:
-          error = "403 Forbidden Error. Check API-KEY"
-          break
-
-        case 400:
-        case 500:
-          if (isErrorWithMessage(result.error.data)) {
-            error = result.error.data.message
-          } else {
-            error = JSON.stringify(result.error.data)
-          }
-          break
-
-        default:
-          error = JSON.stringify(result.error)
-          break
-      }
-      api.dispatch(setAppErrorAC({ error }))
-    }
-
-    // 2. Result code errors
-    if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
-      const messages = (result.data as { messages: string[] }).messages
-      error = messages.length ? messages[0] : error
-      api.dispatch(setAppErrorAC({ error }))
-    }
+    handleError(api, result)
 
     return result
   },
---
+++ src/common/utils/handleError.ts
@@ -0,0 +1,43 @@
+import { setAppErrorAC } from "@/app/app-slice"
+
+import { isErrorWithMessage } from "./isErrorWithMessage"
+import { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from "@reduxjs/toolkit/query/react"
+import { ResultCode } from "@/common/enums/enums.ts"
+
+export const handleError = (
+  api: BaseQueryApi,
+  result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>,
+) => {
+  let error = "Some error occurred"
+
+  if (result.error) {
+    switch (result.error.status) {
+      case "FETCH_ERROR":
+      case "PARSING_ERROR":
+      case "CUSTOM_ERROR":
+        error = result.error.error
+        break
+      case 403:
+        error = "403 Forbidden Error. Check API-KEY"
+        break
+      case 400:
+      case 500:
+        if (isErrorWithMessage(result.error.data)) {
+          error = result.error.data.message
+        } else {
+          error = JSON.stringify(result.error.data)
+        }
+        break
+      default:
+        error = JSON.stringify(result.error)
+        break
+    }
+    api.dispatch(setAppErrorAC({ error }))
+  }
+
+  if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
+    const messages = (result.data as { messages: string[] }).messages
+    error = messages.length ? messages[0] : error
+    api.dispatch(setAppErrorAC({ error }))
+  }
+}
