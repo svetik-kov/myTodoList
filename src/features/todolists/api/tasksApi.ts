e704465961fd083c05edb3f04ab12f74ce3443f2
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-05-02 13:47:48 +0300

    RTK query. Optimistic update.selectCachedArgsForQuery

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -56,7 +56,7 @@ export const tasksApi = baseApi.injectEndpoints({
         method: "PUT",
         body: model,
       }),
-      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled }) {
+      /*async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled }) {
         const patchResult = dispatch(
           tasksApi.util.updateQueryData("getTasks", { todolistId, params: { page: 1 } }, (state) => {
             const index = state.items.findIndex((task) => task.id === taskId)
@@ -69,6 +69,29 @@ export const tasksApi = baseApi.injectEndpoints({
           await queryFulfilled
         } catch {
           patchResult.undo()
+        }*/
+      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled, getState }) {
+        const cachedArgsForQuery = tasksApi.util.selectCachedArgsForQuery(getState(), "getTasks")
+
+        let patchResults: any[] = []
+        cachedArgsForQuery.forEach(({ params }) => {
+          patchResults.push(
+            dispatch(
+              tasksApi.util.updateQueryData("getTasks", { todolistId, params: { page: params.page } }, (state) => {
+                const index = state.items.findIndex((task) => task.id === taskId)
+                if (index !== -1) {
+                  state.items[index] = { ...state.items[index], ...model }
+                }
+              }),
+            ),
+          )
+        })
+        try {
+          await queryFulfilled
+        } catch {
+          patchResults.forEach((patchResult) => {
+            patchResult.undo()
+          })
         }
       },
       invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
